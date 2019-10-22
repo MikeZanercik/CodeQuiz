517b7fcf25bd5ccf04bbb93cd3ca6426d86f6f1f
Mike Zanercik <Michael.zanercik@gmail.com>
2019-10-22 09:59:52 -0400

    refined js file almost completed

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,37 +1,71 @@
-
-var startEl= document.querySelector(".start");
-var timeEl = document.querySelector(".timer");
-var startPageEl = document.querySelector(".startPage");
-var questionEl = document.querySelector(".question");
-var scoreEl = document.querySelector(".score");
-var btnEl = document.querySelector(".Btn");
-var questionHereEl = document.querySelector(".questionHere")
-
-
-questionEl.classList.add("hide");
-
-function startFunction(event){
-    var score =" ";
-    scoreEl.textContent = "Current score: " + score;
-    var secondsLeft = 75;
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left";
-  
-      if(secondsLeft === 0){
-        clearInterval(timerInterval);
-      }
-    }, 1000)
-    startPageEl.classList.add("hide");
-    questionEl.classList.remove("hide");
-}
-
-function BtnFunction(event){
-    var question= "";
-    questionHereEl.textContent = question
-}
+var startEl = document.querySelector("#start");
+var timeEl = document.querySelector("#timer");
+var startPageEl = document.querySelector("#startPage");
+var questionEl = document.querySelector("#question");
+var scoreEl = document.querySelector("#score");
+var scoreboxEl = document.querySelector("#scorebox");
+var answersHereEl = document.querySelector("#choicesHere");
+var questionHereEl = document.querySelector("#questionHere");
+var score, secondsLeft, timerInterval, question, qIndex;
 
 startEl.addEventListener("click", startFunction);
-btnEl.addEventListener("click", BtnFunction);
 
+function startFunction(event) {
+    score = 0, qIndex = 0, secondsLeft = 75;
+    scoreEl.textContent = score;
+    timeEl.textContent = secondsLeft;
+    scoreboxEl.classList.remove("hide");
 
+    startPageEl.classList.add("hide");
+    questionEl.classList.remove("hide");
+
+    startTimer();
+    displayQuestion();
+}
+function startTimer() {
+    clearInterval(timerInterval)
+    timerInterval = setInterval(function () {
+        secondsLeft--;
+
+        if (secondsLeft <= 0) {
+            secondsLeft = 0;
+            endGame();
+        }
+        timeEl.textContent = secondsLeft;
+    }, 1000)
+}
+function displayQuestion() {
+    question = questions[qIndex];
+    questionHereEl.innerText = question.title;
+    answersHereEl.innerHTML = "";
+    for (var i in question.choices) {
+        var button = document.createElement("button");
+        button.setAttribute("value", question.choices[i]);
+        button.classList.add("btn", "btn-default");
+        button.innerText = question.choices[i];
+        button.onclick = handleClick;
+        answersHereEl.appendChild(button)
+    }
+}
+
+function handleClick() {
+    var quess = this.value;
+    if (quess === question.answer) {
+        score++;
+        scoreEl.textContent = score;
+    } else {
+        secondsLeft -= 15;
+    }
+    qIndex++;
+    if (qIndex >= questions.length) {
+        endGame();
+    } else {
+        displayQuestion();
+    }
+}
+function endGame() {
+    clearInterval(timerInterval);
+
+    startPageEl.classList.toggle("hide");
+    questionEl.classList.toggle("hide");
+}
--- assets/questions.js
+++ assets/questions.js
@@ -1,12 +1,12 @@
 var questions = [
     {
-    title: "Commonly used data types DO NOT include:",
-    Choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts" 
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-    title: "The condition in an if /else statment is enclosed within _____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "Parenthese"
+        title: "The condition in an if /else statment is enclosed within _____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "Parenthese"
     }
 ]

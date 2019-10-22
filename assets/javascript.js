c8316cddee69a48f0482af66755f4815c6202f64
Mike Zanercik <Michael.zanercik@gmail.com>
2019-10-22 08:04:45 -0400

    added basic question page and made the start page hide and the questions page show when you click start

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,28 @@
-var score = 0;
-var questions [
-    
-]
+
+var startEl= document.querySelector(".start");
+var timeEl = document.querySelector(".timer");
+var startPageEl = document.querySelector(".startPage");
+var questionEl = document.querySelector(".question");
+var scoreEl = document.querySelector(".score")
+
+
+questionEl.classList.add("hide");
+function startFunction(event){
+    var score =" ";
+    scoreEl.textContent = "Current score: " + score;
+    var secondsLeft = 75;
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left";
+  
+      if(secondsLeft === 0){
+        clearInterval(timerInterval);
+      }
+    }, 1000)
+    startPageEl.classList.add("hide");
+    questionEl.classList.remove("hide");
+}
+
+startEl.addEventListener("click", startFunction);
+
+
---
+++ assets/questions.js
@@ -0,0 +1,12 @@
+var questions = [
+    {
+    title: "Commonly used data types DO NOT include:",
+    Choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts" 
+    },
+    {
+    title: "The condition in an if /else statment is enclosed within _____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "Parenthese"
+    }
+]

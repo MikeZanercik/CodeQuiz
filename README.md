8bbf9a528b3cdbdbc5083eb66b85f2b44149c5a5
Mike Zanercik <Michael.zanercik@gmail.com>
2019-10-22 08:56:05 -0400

    added content to README page

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CodeQuiz
+Contributer:
+    Michael Zanercik
+
+Purpose:
+    This program was designed to test the user knowledge of Javascript in the form of a quiz game.
+    The program asks the user a series of questions with multiple choice answers. A correct answer will add 1 to the users current score and an incorrect answer will remove 15 seconds from the timer and add nothing to the users score.
+
+Deployed on:
+    10/22/2019
--- assets/javascript.js
+++ assets/javascript.js
@@ -3,10 +3,13 @@ var startEl= document.querySelector(".start");
 var timeEl = document.querySelector(".timer");
 var startPageEl = document.querySelector(".startPage");
 var questionEl = document.querySelector(".question");
-var scoreEl = document.querySelector(".score")
+var scoreEl = document.querySelector(".score");
+var btnEl = document.querySelector(".Btn");
+var questionHereEl = document.querySelector(".questionHere")
 
 
 questionEl.classList.add("hide");
+
 function startFunction(event){
     var score =" ";
     scoreEl.textContent = "Current score: " + score;
@@ -23,6 +26,12 @@ function startFunction(event){
     questionEl.classList.remove("hide");
 }
 
+function BtnFunction(event){
+    var question= "";
+    questionHereEl.textContent = question
+}
+
 startEl.addEventListener("click", startFunction);
+btnEl.addEventListener("click", BtnFunction);
 
 
